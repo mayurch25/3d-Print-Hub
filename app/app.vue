26e1c1d4ff93afc17b37766dc11f5885b605d95f
Mayur Chaudhari <mayur.chaudhari@ranium.in>
2026-02-20 10:34:43 +0530

    Added Tailwind CSS and fixed installation

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="min-h-screen flex items-center justify-center bg-black">
+    <h1 class="text-4xl font-bold text-green-500">Tailwind is Working 🚀</h1>
   </div>
 </template>
